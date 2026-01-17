a6a33578af1a41b94a3c86898800c3f59946f8da
sLay1k <138770385+Slavik0911@users.noreply.github.com>
2026-01-17 20:26:37 +0200

    Add base layout and header navigation

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,52 @@
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-[#000E54]">
+       <div className=" px-[2rem] py-8 flex items-center justify-between">
+
+        <div className="leading-tight">
+
+          <NavLink
+            to="/home"
+            className="text-white text-3xl font-semibold">
+            TeachTo<br />Learn
+          </NavLink>
+        </div>
+
+        <nav className="flex items-center text-2xl gap-8 text-white">
+          <NavLink
+            to="/home"
+            className="hover:opacity-70 transition">
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/pro"
+            className="hover:opacity-70 transition">
+            Get pro
+          </NavLink>
+
+          <NavLink
+            to="/search"
+            className="hover:opacity-70 transition">
+            All lessons
+          </NavLink>
+
+          <NavLink
+            to="/login"
+            className="hover:opacity-70 transition">
+            Log in
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className="hover:opacity-70 transition">
+            About us
+          </NavLink>
+        </nav>
+
+      </div>
+    </header>
+  );
+}
--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,22 @@
+// src/router.jsx
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout.jsx";
 import AgeSelect from "./pages/AgeSelect.jsx";
 import Home from "./pages/Home.jsx";
 import LessonSearch from "./pages/LessonSearch.jsx";
 import LessonDetails from "./pages/LessonDetails.jsx";
-import { Routes, Route } from "react-router-dom";
 
-
-function Router() {
+export default function Router() {
   return (
     <Routes>
+      {/* Without Layout element */}
       <Route path="/" element={<AgeSelect />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/search" element={<LessonSearch />} />
-      <Route path="/lessons/:id" element={<LessonDetails />} />
+
+      <Route element={<Layout />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/search" element={<LessonSearch />} />
+        <Route path="/lessons/:id" element={<LessonDetails />} />
+      </Route>
     </Routes>
   );
 }
-
-export default Router;

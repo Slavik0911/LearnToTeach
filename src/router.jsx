1f2e32ac023a6d2830f98eecb7c851c9061f987b
Slavik0911 <pilipuaroslav21@gmail.com>
2026-03-09 16:35:40 +0200

    refactor: reorganize pages into admin/auth/profile folders

--- src/router.jsx
+++ src/router.jsx
@@ -1,27 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout.jsx";
 
-import AgeSelect from "@/pages/AgeSelect";
 import Home from "@/pages/Home";
 import LessonSearch from "@/pages/LessonSearch";
 import LessonDetails from "@/pages/LessonDetails";
-import Profile from "@/pages/profiles/Profile";
-import Admin from "@/pages/profiles/Admin";
-import AdminGuard from "@/pages/profiles/AdminGuard";
-import LogIn from "@/pages/profiles/LogIn";
-import SignUp from "@/pages/profiles/Signup";
-import ForgotPassword from "@/pages/profiles/ForgotPassword";
-import AddLesson from "@/pages/AddLesson";
-import SavedLessons from "@/pages/profiles/SavedLessons.jsx";
+import Profile from "@/pages/profile/Profile";
+import Admin from "@/pages/admin/Admin";
+import AdminGuard from "@/pages/admin/AdminGuard";
+import LogIn from "@/pages/auth/LogIn";
+import SignUp from "@/pages/auth/SignUp";
+import ForgotPassword from "@/pages/auth/ForgotPassword";
+import AddLesson from "@/pages/admin/AddLesson";
+import SavedLessons from "@/pages/profile/SavedLessons.jsx";
 
 // This component is responsible for defining all the routes in the application and 
 // rendering the appropriate components based on the URL
 export default function Router() {
   return (
     <Routes>
-      {/* without Layout */}
-      {/* <Route path="/" element={<AgeSelect />} /> */}
-      
       {/* with Layout */}
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
